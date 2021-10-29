5edfc15fb41cc02e761c564719348bfc54d68205
anson-li <ansonli.94@gmail.com>
2021-10-28 18:47:21 -0600

    Refactored implementation to use hooks

--- randyhub/src/App.js
+++ randyhub/src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useRef } from 'react';
+import React, { Suspense } from 'react';
 import {
   BrowserRouter, Route, Switch,
 } from 'react-router-dom';
@@ -6,24 +6,21 @@ import Main from './routes/Main';
 import './index.css';
 import CustomCursor from './common/CustomCursor';
 
-const App = () => {
-  const cursor = useRef();
+const App = () => (
+  <div id="app">
+    <CustomCursor />
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route component={Main} exact path="/" />
+          <Route component={React.lazy(() => import('./routes/SnackOfChampions'))} path="/snack-of-champions" />
+          <Route component={React.lazy(() => import('./routes/CookingWithRandy'))} path="/cooking-with-randy" />
+          <Route component={React.lazy(() => import('./routes/GitRepo'))} path="/git-repo" />
+          <Route component={React.lazy(() => import('./routes/CovidCounter'))} path="/covid-counter" />
+        </Switch>
+      </Suspense>
+    </BrowserRouter>
+  </div>
+);
 
-  return (
-    <div id="app">
-      <CustomCursor ref={cursor.current} />
-      <BrowserRouter>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Switch>
-            <Route component={Main} exact path="/" />
-            <Route component={React.lazy(() => import('./routes/SnackOfChampions'))} path="/snack-of-champions" />
-            <Route component={React.lazy(() => import('./routes/CookingWithRandy'))} path="/cooking-with-randy" />
-            <Route component={React.lazy(() => import('./routes/GitRepo'))} path="/git-repo" />
-            <Route component={React.lazy(() => import('./routes/CovidCounter'))} path="/covid-counter" />
-          </Switch>
-        </Suspense>
-      </BrowserRouter>
-    </div>
-  );
-};
 export default App;
--- randyhub/src/common/CustomCursor/index.js
+++ randyhub/src/common/CustomCursor/index.js
@@ -2,54 +2,28 @@ import {
   gsap,
 } from 'gsap';
 import React, {
-  PureComponent,
+  useEffect, useState, useRef,
 } from 'react';
 import sampleImage from '../../assets/mashedpotatoes.jpg';
 import sampleMouth from '../../assets/coolhead.png';
 import './style.scss';
 
-class CustomCursor extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.moveCircle = this.moveCircle.bind(this);
-    this.setScaleAndRotation = this.setScaleAndRotation.bind(this);
-    this.setMouthScaleAndRotation = this.setMouthScaleAndRotation.bind(this);
-    this.toggleSlider = this.toggleSlider.bind(this);
-    this.state = {
-      cursorToggled: false,
-    };
-  }
+const CustomCursor = () => {
+  const [cursorToggled, setCursorToggled] = useState(false);
+  const circle = useRef(null);
+  const follow = useRef(null);
+  const mouth = useRef(null);
 
-  componentDidMount() {
-    window.addEventListener('mousemove', this.moveCircle);
-    // window.addEventListener('scroll', this.onScroll);
-  }
-
-  onScroll(event) {
-    if (this.circle) {
-      gsap.to(this.circle, 0.1, {
-        x: event.x,
-        y: event.y,
-      });
-    }
-    if (this.follow) {
-      gsap.to(this.follow, 0.7, {
-        x: event.x,
-        y: event.y,
-      });
-    }
-  }
-
-  setScaleAndRotation(event) {
-    if (this.follow) {
-      const target = this.follow.getBoundingClientRect();
+  function setScaleAndRotation(event) {
+    if (follow.current) {
+      const target = follow.current.getBoundingClientRect();
       const centerX = target.x + target.width / 2;
       const centerY = target.y + target.height / 2;
       const scale = Math.min(
         Math.sqrt(((event.y - centerY) ** 2) + ((event.x - centerX) ** 2)) * 0.002,
         1,
       );
-      gsap.set(this.follow, {
+      gsap.set(follow.current, {
         rotation: Math.atan2(
           (event.y - centerY),
           (event.x - centerX),
@@ -59,16 +33,16 @@ class CustomCursor extends PureComponent {
     }
   }
 
-  setMouthScaleAndRotation(event) {
-    if (this.mouth) {
-      const target = this.mouth.getBoundingClientRect();
+  function setMouthScaleAndRotation(event) {
+    if (mouth) {
+      const target = mouth.current.getBoundingClientRect();
       const centerX = target.x + target.width / 2;
       const centerY = target.y + target.height / 2;
       const scale = Math.min(
         Math.sqrt(((event.y - centerY) ** 2) + ((event.x - centerX) ** 2)) * 0.004,
         1.5,
       );
-      gsap.set(this.mouth, {
+      gsap.set(mouth.current, {
         rotation: Math.atan2(
           (event.y - centerY),
           (event.x - centerX),
@@ -78,80 +52,73 @@ class CustomCursor extends PureComponent {
     }
   }
 
-  toggleSlider() {
-    this.setState((prevState) => ({
-      cursorToggled: !prevState.cursorToggled,
-    }));
-  }
-
-  moveCircle(event) {
-    if (event) {
-      if (this.circle) {
-        gsap.to(this.circle, 0.1, {
-          x: event.x,
-          y: event.y,
-          z: 0.1,
-        });
-      }
-      if (this.follow) {
-        gsap.to(this.follow, 1.0, {
-          x: event.x,
-          y: event.y,
-          z: 0.1,
-          onUpdate: this.setScaleAndRotation,
-          onUpdateParams: [event],
-        });
-      }
-      if (this.mouth) {
-        gsap.to(this.mouth, 2.0, {
-          x: event.x,
-          y: event.y,
-          z: 0.1,
-          onUpdate: this.setMouthScaleAndRotation,
-          onUpdateParams: [event],
-        });
+  useEffect(() => {
+    function moveCircle(event) {
+      if (event) {
+        if (circle.current) {
+          gsap.to(circle.current, 0.1, {
+            x: event.x,
+            y: event.y,
+            z: 0.1,
+          });
+        }
+        if (follow.current) {
+          gsap.to(follow.current, 1.0, {
+            x: event.x,
+            y: event.y,
+            z: 0.1,
+            onUpdate: setScaleAndRotation,
+            onUpdateParams: [event],
+          });
+        }
+        if (mouth.current) {
+          gsap.to(mouth.current, 2.0, {
+            x: event.x,
+            y: event.y,
+            z: 0.1,
+            onUpdate: setMouthScaleAndRotation,
+            onUpdateParams: [event],
+          });
+        }
       }
     }
+
+    window.addEventListener('mousemove', moveCircle);
+  }, []);
+
+  function toggleSlider() {
+    setCursorToggled(!cursorToggled);
   }
 
-  render() {
-    const { cursorToggled } = this.state;
-    return (
-      <>
-        <div className="cursor">
-          <div
-            className={`cursor-circle ${!cursorToggled ? 'disabled' : ''}`}
-            ref={(ref) => {
-              this.circle = ref;
-            }}
-          />
-          <img
-            src={sampleImage}
-            alt="Img"
-            className={`cursor-circle-follow ${!cursorToggled ? 'disabled' : ''}`}
-            ref={(ref) => {
-              this.follow = ref;
-            }}
-          />
-          <img
-            src={sampleMouth}
-            alt="Img"
-            className={`cursor-circle-follow ${!cursorToggled ? 'disabled' : ''}`}
-            ref={(ref) => {
-              this.mouth = ref;
-            }}
-          />
-        </div>
-        <div className="toggle-cursor">
-          <img className="slider-hint" src={sampleMouth} alt="Click the toggle..." />
-          <label htmlFor="slider-checkbox" className="switch">
-            <input onClick={this.toggleSlider} id="slider-checkbox" type="checkbox" />
-            <span className="slider round" />
-          </label>
-        </div>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <div className="cursor">
+        <div
+          className={`cursor-circle ${!cursorToggled ? 'disabled' : ''}`}
+          ref={circle}
+        />
+        <img
+          src={sampleImage}
+          alt="Img"
+          className={`cursor-circle-follow ${!cursorToggled ? 'disabled' : ''}`}
+          ref={follow}
+        />
+        <img
+          src={sampleMouth}
+          alt="Img"
+          className={`cursor-circle-follow ${!cursorToggled ? 'disabled' : ''}`}
+          ref={mouth}
+        />
+      </div>
+      <div className="toggle-cursor">
+        <img className="slider-hint" src={sampleMouth} alt="Click the toggle..." />
+        <label htmlFor="slider-checkbox" className="switch">
+          <input onClick={toggleSlider} id="slider-checkbox" type="checkbox" />
+          <span className="slider round" />
+        </label>
+      </div>
+    </>
+  );
+};
 
 export default CustomCursor;
